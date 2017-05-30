4aba2585c9e818f87e96caa254ef3ef693dc2dc9
HyechurnJang <hyjang@cisco.com>
2017-05-30 17:01:50 +0900

    add server

---
+++ server.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 2017. 5. 30.
+@author: HyechurnJang
+'''
+
+import pygics
+
+pygics.server('0.0.0.0',
+              80,
+              modules=['../sparkbot'])
